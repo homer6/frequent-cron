e39ee756bdc2f9170ec1e3dee72ef953a512d6ac
Steve Sperandeo <steve.sperandeo@gmail.com>
2026-04-04 18:41:56 -0700

    Fix Windows build: replace WIN32_LEAN_AND_MEAN with _WINSOCKAPI_
    WIN32_LEAN_AND_MEAN strips too many headers including <process.h>,
    breaking _beginthreadex (Boost ASIO) and _getpid (pid_file).
    _WINSOCKAPI_ specifically prevents WinSock 1 inclusion without
    stripping other Windows SDK headers.

--- include/executor.h
+++ include/executor.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#ifdef _WIN32
-    #include <process.h>  // _beginthreadex, needed before boost/asio with WIN32_LEAN_AND_MEAN
-#endif
-
 #include <string>
 #include <chrono>
 #include <memory>
